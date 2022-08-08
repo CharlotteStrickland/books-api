edce4efce1022d3be32f522990c38a72b2687ee8
Charlotte Strickland <charlottemarie81@gmail.com>
2022-08-08 15:50:45 -0400

    Added coding to server.js

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const { use } = require('./controllers/booksController')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const MONGO_URI = process.env.MONGO_URI
+app.use(express.json())
+app.use(cors())
+
+//database
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log(`Mongo connected: ${MONGO_URI}`)})
+
+// Controllers
+const books_controller = require('./controllers/booksController')
+app.use('/books', booksController)
 
 // ROUTES
 app.get('/', (req, res) => {
